a9b9eab912bbd732e8b28ea7f887f0873776e97e
nativealien <oskar@assarsson.nu>
2024-05-16 09:23:11 +0200

    node addresses can now be regged, and stored

--- controllers/nodeController.mjs
+++ controllers/nodeController.mjs
@@ -1,14 +1,24 @@
 import { blockchain } from "../utilities/initiate.mjs"
-import { loadJson } from "../data/fileManager.mjs"
+import { loadJson, saveJson } from "../data/fileManager.mjs"
 
 const getNodes = (req, res, next) => {
     res.status(200).json({data: blockchain.nodes})
 }
-const registerNode = (req, res, next) => {
-    const newUrl = req.body.node
-    
+const registerNode = async (req, res, next) => {
 
-    res.status(201).json({message: 'Hej'})
+    const folderPath = '/data/nodes/urls.json'
+    const messageObj = { message: ''}
+
+    const newUrl = req.body.node
+    const urlList = await loadJson(folderPath)
+    if(!urlList[0].nodes.includes(newUrl)){
+
+        urlList[0].nodes.push(newUrl)
+        await saveJson(folderPath, urlList)
+        messageObj.message = `Node ${newUrl} added succesfully!`
+    }else messageObj.message = `Node ${newUrl} was already registerd!`
+
+    res.status(201).json(messageObj)
     
 }
 
--- utilities/initiate.mjs
+++ utilities/initiate.mjs
@@ -6,13 +6,13 @@ export const blockchain = new Blockchain()
 export const initChain = async () => {
 
     const nodesJson = await loadJson('/data/nodes/urls.json')
-    const nodes = [...JSON.parse(nodesJson)]
+    const nodes = [...nodesJson]
     const urls = nodes[0].nodes
     blockchain.nodes = urls.filter( url => url !== blockchain.url)
 
     const chainJson = await loadJson('/data/blockchain/chain.json')
     if(chainJson === '') saveJson('/data/blockchain/chain.json', blockchain.chain)
-    else blockchain.chain = [...JSON.parse(chainJson)]
+    else blockchain.chain = [...chainJson]
 
 }
 
